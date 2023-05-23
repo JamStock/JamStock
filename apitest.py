758810cbec020500bd88895caab69a6c17cf5c55
HaekyungSeong <haekyungs95@gmail.com>
2023-05-23 09:14:32 +0900

    python 으로 대신증권 api 불러오는 코드 테스트

---
+++ apitest.py
@@ -0,0 +1,24 @@
+#종목정보 가져오기(최초)
+import win32com.client
+import pandas as pd 
+
+rows = list()
+
+CPE_MARKET_KIND = {'KOSPI':1,  'KOSDAQ':2}
+instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+
+for key, value in CPE_MARKET_KIND.items():
+    codeList = instCpCodeMgr.GetStockListByMarket(value)
+    for code in codeList:
+        name = instCpCodeMgr.CodeToName(code)
+        sectionKind = instCpCodeMgr.GetStockSectionKind(code)
+        row = [code, name, key, sectionKind]
+        rows.append(row)
+
+print('모든 종목을 불러왔습니다')
+
+stockitems = pd.DataFrame(data= rows, columns=['code','name', 'section','sectionKind'])
+stockitems.loc[stockitems['sectionKind'] == 10, 'section'] =   'ETF'
+stockitems.to_csv('stockitems.csv', index=False)
+
+print('파일을 저장하였습니다.')
