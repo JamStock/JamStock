0306916e55925b9b84ec79323f1b8a92e002dd1b
HaekyungSeong <haekyungs95@gmail.com>
2023-06-07 17:30:04 +0900

    종목 코드 DB 저장용 코드- 코드 및 이름 저장 완료

---
+++ jam-project/Utils/DB/DB_Stockinsert.tsx
@@ -0,0 +1,53 @@
+// import { debug } from 'console'
+import mysql from 'mysql2'
+import {spawn} from 'child_process'
+import express, {Request, Response} from 'express'
+import fs from 'fs'
+
+// 버퍼로 나옴
+let datas = fs.readFileSync('./stockInfo.json')
+
+// 버퍼를 풀어준다.(한글로)
+// console.log(datas.toString('utf-8'))
+// console.log(typeof datas.toString('utf-8'))
+
+// json 을 파싱한다. 그러면 이제 객체 완성.
+// console.log(JSON.parse(datas.toString('utf-8'))['kospi'])
+// console.log(JSON.parse(datas))
+
+let dataObject = JSON.parse(datas.toString('utf-8'))['kosdaq']
+// console.log(dataObject)
+
+export const DBInfo = mysql.createConnection({
+  host:'192.168.100.77',
+  database:'jamstock',
+  user:'euni',
+  password:'0000',
+  port:3306
+})
+
+DBInfo.connect(()=>{
+console.log('DB connect')
+})
+
+for(let companyname in dataObject){
+  DBInfo.query(`insert into stockdata(code, companyname) value('${dataObject[companyname]}', '${companyname}')`, (err, result)=>{
+    if(err) console.error(err)
+    console.log('kosdaq success')
+  })
+
+}
+
+DBInfo.end()
+
+// const app=express()
+
+// app.get('/',(req:Request, res:Response)=>{
+  // const result = spawn('python',['../../Models/stock/stockCodeName.py'])
+
+  // result.stdout.on('data',(data)=>{
+  //   res.send('뭔데')
+  // })
+// }).listen(2222,()=>{
+  // console.log('connecting')
+// })
