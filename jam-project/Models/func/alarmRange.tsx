565b51e5df6a0ff998bfbd518097711262239fd7
HaekyungSeong <haekyungs95@gmail.com>
2023-06-11 12:33:25 +0900

    비교연산자 오류 수정

--- jam-project/Models/func/alarmRange.tsx
+++ jam-project/Models/func/alarmRange.tsx
@@ -6,53 +6,53 @@ interface AlarmRange {
 }
 
 // ! 알람 금액 기준에 따른 안내문구 및 이미지정보
-export const alarmRange = (price: number): AlarmRange => {
-  if (price >= 200 || price < 1200) {
+export const AlarmRange = (price: number): AlarmRange => {
+  if (price >= 200 && price < 1200) {
     return {
       item: '춥파춥스 1개를 구매 가능하세요!',
       url: '../../Resource/Alarm/chupa.png'
     }
-  } else if (price >= 1200 || price < 4000) {
+  } else if (price >= 1200 && price < 4000) {
     return {
       item: 'PC방 1시간을 이용 할 수 있으세요!',
       url: '../../Resource/Alarm/pc.png'
     }
-  } else if (price >= 4000 || price < 8000) {
+  } else if (price >= 4000 && price < 8000) {
     return {
       item: '아메리카노 한 잔을 드실 수 있으세요!',
       url: '../../Resource/Alarm/coffee.png'
     }
-  } else if (price >= 8000 || price < 15000) {
+  } else if (price >= 8000 && price < 15000) {
     return {
       item: '아이스크림 한 통을 구매 가능하세요!',
       url: '../../Resource/Alarm/icecream.png'
     }
-  } else if (price >= 15000 || price < 25000) {
+  } else if (price >= 15000 && price < 25000) {
     return {
       item: '혼자 영화 한편 보실 수 있으세요!',
       url: '../../Resource/Alarm/movie.png'
     }
-  } else if (price >= 25000 || price < 50000) {
+  } else if (price >= 25000 && price < 50000) {
     return {
       item: '치킨 한마리를 드실 수 있으세요!',
       url: '../../Resource/Alarm/chicken.png'
     }
-  } else if (price >= 50000 || price < 100000) {
+  } else if (price >= 50000 && price < 100000) {
     return {
       item: '양주 한 병을 구입할 수 있으세요!',
       url: '../../Resource/Alarm/jackdaniels.png'
     }
-  } else if (price >= 100000 || price < 200000) {
+  } else if (price >= 100000 && price < 200000) {
     return {
       item: '메이커 신발을 구입 할 수 있으세요!',
       url: '../../Resource/Alarm/shoes.png'
     }
-  } else if (price >= 200000 || price < 500000) {
+  } else if (price >= 200000 && price < 500000) {
     return {
       item: '킹크랩 한 마리를 먹을 수 있으세요!',
       url: '../../Resource/Alarm/kingcrab.png'
     }
-  } else if (price >= 200000 || price < 500000) {
+  } else if (price >= 200000 && price < 500000) {
     return {
       item: '명품 지갑을 구입 할 수 있으세요!',
       url: '../../Resource/Alarm/wallet.png'
